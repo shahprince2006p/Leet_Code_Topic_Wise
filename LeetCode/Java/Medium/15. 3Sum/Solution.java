5dc062541abc2d2525ea7123ce5f2d7eb2035a3d
Prince_Shah <shahlaxmi1986@gmail.com>
2026-08-13 22:28:15 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/15. 3Sum/Solution.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> ans =new ArrayList<>();
+        int sum=0;
+
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+            int left=i+1;
+            int right=nums.length-1;
+
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+
+            while(left<right){
+                sum=nums[i]+nums[left]+nums[right];
+
+                if(sum==0){
+                    ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    left++;
+                    right--;
+
+                    while(left<right && nums[left]==nums[left-1]){
+                        left++;
+                    }
+
+                      while(left<right && nums[right]==nums[right+1]){
+                        right--;
+                    }
+
+                }else if(sum<0){
+                    left++;
+                }else{
+                    right--;
+                }
+            }
+        }
+        return ans;
+        
+    }
+}
