c52dc303373368c3d123a690d2985c3e8e1ba3ef
Prince_Shah <shahlaxmi1986@gmail.com>
2026-08-17 10:47:35 +0530

    Add Java Solution to Patterns/Binary Search

---
+++ LeetCode/Patterns/Binary Search/1646. Kth Missing Positive Number/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+       int l=0;
+       int h=arr.length-1;
+
+       while(l<=34h){
+        int mid=l+(h-l)/2;
+        int missing=arr[mid]-(mid+1);
+
+        if(missing<k){
+            l=mid+1;
+        }else{
+            h=mid-1;
+        }
+       }
+       return l+k;
+    }
+}
