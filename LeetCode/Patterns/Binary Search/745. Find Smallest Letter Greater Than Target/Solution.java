158176a369f82b3747ae63c7e9d2f75dd9643821
Prince_Shah <shahlaxmi1986@gmail.com>
2026-08-18 10:44:55 +0530

    Add Java Solution to Patterns/Binary Search

---
+++ LeetCode/Patterns/Binary Search/745. Find Smallest Letter Greater Than Target/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+
+        int l=0;
+        int h=letters.length-1;
+
+        while(l<=h){
+            int mid=l+(h-l)/2;
+
+            if(letters[mid]<=target){
+                l=mid+1;
+            }else{
+                h=mid-1;
+            }
+        }
+        return letters[l%letters.length];  
+    }
+}
