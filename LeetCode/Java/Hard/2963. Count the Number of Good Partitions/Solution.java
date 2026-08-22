a44bf4f64264595aa1909f1554e4cf4a517ced8a
Prince_Shah <shahlaxmi1986@gmail.com>
2026-08-22 10:57:09 +0530

    Add Hard Java Solution from LeetCode

--- LeetCode/Java/Hard/2963. Count the Number of Good Partitions/Solution.java
+++ LeetCode/Java/Hard/2963. Count the Number of Good Partitions/Solution.java
@@ -11,14 +11,14 @@ class Solution {
         int count = 0;
         int end = 0;
 
-        for(int i = 0; i < nums.length; i++) {
+        // for(int i = 0; i < nums.length; i++) {
 
-            end = Math.max(end, last.get(nums[i]));
+        //     end = Math.max(end, last.get(nums[i]));
 
-            if(i == end) {
-                count++;
-            }
-        }
+        //     if(i == end) {
+        //         count++;
+        //     }
+        // }
 
         return (int)Math.pow(2, count - 1);
     }
